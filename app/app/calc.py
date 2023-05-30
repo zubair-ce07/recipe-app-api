45212cc73dade6cb967b91b64d62e7b633e805c2
Muhammad Zubair <syedzubairtahir12@gmail.com>
2023-05-30 13:34:28 +0500

    iupdated docker file

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator Function
+"""
+
+def add(x, y):
+    
+    return x+y
+
+def subtract_number(x, y):
+    
+    return abs(x-y)
---
+++ app/app/tests.py
@@ -0,0 +1,20 @@
+"""
+Sample test
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+
+    def test_add_nnumbers(self):
+        """Test adding numbers togther"""
+        res = calc.add(5, 6)
+
+        self.assertEqual(res, 11)
+
+    def test_subtract_number(self):
+
+        res = calc.subtract_number(10, 15)
+        self.assertEqual(res, 5)
